10c19b18f14bfdc31ad267c5e8f750be55bf1cd7
Kaalleen <reni@allenka.de>
2024-03-31 20:03:43 +0200

    add buffer option to outline extension handle holes better

--- lib/extensions/outline.py
+++ lib/extensions/outline.py
@@ -8,6 +8,7 @@ from shapely import concave_hull
 from shapely.geometry import LineString, MultiPolygon
 
 from ..i18n import _
+from ..svg import PIXELS_PER_MM
 from ..svg.tags import SVG_PATH_TAG
 from .base import InkstitchExtension
 
@@ -17,6 +18,7 @@ class Outline(InkstitchExtension):
         InkstitchExtension.__init__(self, *args, **kwargs)
         self.arg_parser.add_argument("-r", "--ratio", type=float, default=0.0, dest="ratio")
         self.arg_parser.add_argument("-a", "--allow-holes", type=inkex.Boolean, default=False, dest="allow_holes")
+        self.arg_parser.add_argument("-b", "--buffer", type=float, default=0.0, dest="buffer")
 
     def effect(self):
         if not self.svg.selection:
@@ -32,19 +34,25 @@ class Outline(InkstitchExtension):
                 self.element_to_outline(element)
             return
 
+        shape_buffer = max(self.options.buffer * PIXELS_PER_MM, 0.001)
         path = element.get_path()
         path = path.end_points
-        hull = concave_hull(LineString(path), ratio=self.options.ratio, allow_holes=self.options.allow_holes)
+        shape = LineString(path).buffer(shape_buffer)
+        hull = concave_hull(
+            shape,
+            ratio=self.options.ratio,
+            allow_holes=self.options.allow_holes
+        )
         if isinstance(hull, LineString):
             return
 
         if not isinstance(hull, MultiPolygon):
             hull = MultiPolygon([hull])
+
         d = ''
         for geom in hull.geoms:
-            d += 'M '
-            for x, y in geom.exterior.coords:
-                d += f'{x}, {y} '
-            d += "Z"
-
+            d += str(inkex.Path(geom.exterior.coords))
+            if self.options.allow_holes:
+                for interior in geom.interiors:
+                    d += str(inkex.Path(interior.coords))
         element.set('d', d)
